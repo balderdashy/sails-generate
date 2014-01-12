ae5478e817c3d5c85aff61edbe793c26c43a8cc1
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-11 19:25:40 -0600

    Cleanup error output sample in stub tests.g

--- test/integration/generator.test.js
+++ test/integration/generator.test.js
@@ -1,41 +1,43 @@
 var _ = require('lodash');
 var generate = require('root-require')('lib');
 
-var scope = {};
-
 generate({
 	targets: {
 		'.': 'generator'
 	}
-}, scope, function (err) {
-	if (err) throw err;
-	digestScopeOutput(scope);
-});
+}, {}, handleOutput());
 
 
 
-/**
- * Call `handleScopeOutputEntry` for each item in
- * the scope output queue (e.g. log response)
- * 
- * @param  {[type]} scope [description]
- * @return {[type]}       [description]
- */
-function digestScopeOutput(scope) {
-	_.each(scope.output, function (entry) {
-		if (typeof entry === 'function') {
-			entry = entry(scope);
+function handleOutput () {
+	var log = new (require('captains-log'))();
+
+	/**
+	 * 
+	 * @param  {[type]} err    [description]
+	 * @param  {[type]} output [description]
+	 * @return {[type]}        [description]
+	 */
+	return function handleOutput (err, output) {
+		if (err) {
+			var outputFn = _.bind(writeOutput, null, 'error');
+			var errOutput = err instanceof Error?
+				String(err).replace(/^Error:\s/,'')
+				: String(err);
+			return _.each(errOutput.split('\n'), outputFn);
 		}
-		handleScopeOutputEntry(entry);
-	});
-}
+		return _.each(output, writeOutput);
+	};
 
-/**
- * Overridable function to emit output.
- * 
- * @param  {[type]} entry [description]
- * @return {[type]}       [description]
- */
-function handleScopeOutputEntry (entry) {
-	console.log(entry);
+	/**
+	 * Overridable function to emit output.
+	 * 
+	 * @param  {String} channel [description]
+	 * @param  {[type]} entry [description]
+	 * @return {[type]}       [description]
+	 */
+	function writeOutput (channel, output) {
+		channel = channel || 'info';
+		log[channel](output);
+	}
 }
