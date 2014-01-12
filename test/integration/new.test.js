f53776b703563e613d924d38d5818a802e1071ba
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-11 23:51:01 -0600

    Added new test

---
+++ test/integration/new.test.js
@@ -0,0 +1,42 @@
+var _ = require('lodash');
+var generate = require('root-require')('lib');
+
+
+
+
+
+
+
+generate({
+	targets: {
+		'.': 'new'
+	}
+}, {}, logReporter());
+
+
+
+
+
+
+/**
+ * Log reporter
+ */
+function logReporter () {
+	var log = new (require('captains-log'))();
+
+	/**
+	 * 
+	 * @param  {[type]} err    [description]
+	 * @param  {[type]} output [description]
+	 * @return {[type]}        [description]
+	 */
+	return function (err, output) {
+		if (err) {
+			var errOutput = err instanceof Error?
+				String(err).replace(/^Error:\s/,'')
+				: String(err);
+			return _.each(errOutput.split('\n'), function (item) {log.error(item);});
+		}
+		return _.each(output, function(item) {log.info(item);});
+	};
+}
